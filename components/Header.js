510b796fbbe6dfe094329948f5ebec058188fc6e
123MayankSharma <Mayanksharma922001@gmail.com>
2021-09-09 01:56:33 +0530

    created and edited next.config.js

---
+++ components/Header.js
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+
+
+
+
+const Header = () => {
+    return (<>
+        <header className="text-left">
+            this is the header
+        <Image src="https://img.icons8.com/ios/2x/hulu.png" width={100} height={100}/>
+        </header>
+
+
+    </>)
+
+
+
+}
+
+export default Header
