51526dbc53a3931e2a837009a1ea483814099b10
123MayankSharma <Mayanksharma922001@gmail.com>
2021-09-09 19:39:26 +0530

    encountered issue with responsiveness so a savepoint

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,28 @@
 import Image from 'next/image'
 import HeaderIcons from "./HeaderIcons.js"
-import { BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon, UserIcon} from '@heroicons/react/outline'
+import { BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon, UserIcon } from '@heroicons/react/outline'
 
 
 
 const Header = () => {
     return (<>
-        <header className="text-left">
-       <div className="flex flex-col items-stretch sm:place-items-stretch">
-        <HeaderIcons title='Home' Icon={HomeIcon}/>
-        
-        <Image 
-        className="object-contain"
-        src="https://img.icons8.com/ios/2x/hulu.png" 
-        width={200} height={100}/>
-        </div>
+        <header className="text-left flex flex-col">
+            <div className="flex">
+                <HeaderIcons title='Home' Icon={HomeIcon} />
+                <HeaderIcons title='Trending' Icon={LightningBoltIcon} />
+                <HeaderIcons title='Verified' Icon={BadgeCheckIcon} />
+                <HeaderIcons title='Collections' Icon={CollectionIcon} />
+                <HeaderIcons title='Search' Icon={SearchIcon} />
+                <HeaderIcons title='Account' Icon={UserIcon} />
+               </div>
+
+
+
+              <div  className="flex flex-col items-center -mt-6 sm:items-end sm:-mt-20">
+                <Image
+                    src="https://img.icons8.com/color/2x/hulu.png"
+                    width={100} height={100} />
+            </div>
         </header>
 
 
